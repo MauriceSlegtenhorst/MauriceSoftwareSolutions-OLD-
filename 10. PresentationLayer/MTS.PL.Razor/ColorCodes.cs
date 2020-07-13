830efe51c8b23a16a218f5d9faff0dc71d8bc6f2
Maurice Slegtenhorst <m.slegtenhorst@LIVE.NL>
2020-07-13 07:17:41 +0200

    Not much cloud, more logo adding and theming

---
+++ 10. PresentationLayer/MTS.PL.Razor/ColorCodes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MTS.PL.UserInterface
+{
+    public class ColorCodes
+    {
+        public const string BLUE = "#0466d6";
+        public const string SMOKE_WHITE = "#eeeeee";
+        public const string LIGHT_GREEN = "#5baab3";
+        public const string ORANGE = "#b43e07";
+        public const string BLACK = "#000000";
+        public const string GRAY = "#393e46";
+        public const string DARK_GRAY = "#201f1f";
+    }
+}
