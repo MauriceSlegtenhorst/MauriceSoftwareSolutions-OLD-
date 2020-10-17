71fc15e3101f7e67eae3ae6ed4d9c89211c98dc3
Maurice Slegtenhorst <m.slegtenhorst@LIVE.NL>
2020-10-17 15:35:56 +0200

    Starting fresh

---
+++ MTS.BL.Logic/Credit/Read.cs
@@ -0,0 +1,14 @@
+﻿using MTS.PL.Infra.Entities.Standard.Credit;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MTS.BL.Logic.Credit
+{
+    public sealed class Read
+    {
+        public async Task<ICollection<PLCreditCategory>> ReadCreditCategories()
+        {
+
+        }
+    }
+}
